9174f2b03e0922260ac22d7928f012f2b2bd6648
Rio1210 <39292603+Rio1210@users.noreply.github.com>
2021-02-18 03:21:21 -0500

    Add files via upload

---
+++ wild-ryde-site/js/config.js
@@ -0,0 +1,19 @@
+window._config = {
+
+    cognito: {
+
+        userPoolId: 'us-east-1_Qhc4JOojo', // e.g. us-east-2_uXboG5pAb
+
+        userPoolClientId: '52thu4hc3f24ddg1djnie0k9e9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+
+        region: 'us-east-1' // e.g. us-east-2
+
+    },
+
+    api: {
+
+        invokeUrl: 'https://ai4x3r15p4.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
+
+    }
+
+};
